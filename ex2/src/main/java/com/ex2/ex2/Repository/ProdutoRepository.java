3fb6d3fc1ce8a422a19ffaf608bfd0b5017fc55e
Gabriel Maia <gabrielhenrikdamaia@gmail.com>
2026-03-24 17:39:24 -0300

    feat: add repository

---
+++ ex2/src/main/java/com/ex2/ex2/Repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.ex2.ex2.Repository;
+
+
+import com.ex2.ex2.Entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
